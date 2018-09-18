868676fec833097e22192f35d73287c84a64816f
AxelRj13 <axel.raharja1@gmail.com>
2018-09-18 15:33:47 +0700

    Template email untuk aktivasi account

---
+++ mail/account_activation.php
@@ -0,0 +1,20 @@
+<?php
+use yii\helpers\Html;
+
+/* @var $this yii\web\View */
+/* @var $email */
+/* @var $full_name */
+/* @var $user common\models\User */
+
+$activationLink = Yii::$app->urlManager->createAbsoluteUrl(['site/activate-account', 'token' => $user->account_activation_token]);
+?>
+
+<div class="activation">
+    <p>Hello <?= $full_name ?>,</p>
+
+    <p>You've been registered with your email <?= $email ?></p>
+
+    <p>Please follow the link below to activate your <?= Yii::$app->name ?> Account:</p>
+
+    <p><?= Html::a(Html::encode($activationLink), $activationLink) ?></p>
+</div>
