ec18571843440785cff28fcf5aa27e1461560d62
AxelRj13 <axel.raharja1@gmail.com>
2018-11-08 17:41:04 +0700

    Penambahan message

--- messages/id-ID/app.php
+++ messages/id-ID/app.php
@@ -82,6 +82,8 @@ return [
     'First Name' => 'Nama Depan',
     'Last Name' => 'Nama Belakang',
     'About Me' => 'Tentang Saya',
+    'Contact' => 'Kontak',
+    'Primary Contact' => 'Kontak Utama',
     'Is Primary Contact' => 'Kontak Utama',
     'Business Category' => 'Kategori Bisnis',
     'Product Category' => 'Kategori Menu',
