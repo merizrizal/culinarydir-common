eb8dbd5d9e326c054d0f4bdcccaef9c6e591ef3a
AxelRj13 <axel.raharja1@gmail.com>
2018-11-27 18:50:31 +0700

    Penambahan message

--- messages/id-ID/app.php
+++ messages/id-ID/app.php
@@ -243,10 +243,10 @@ return [
     'Order List' => 'Daftar Pesanan',
     'Amount' => 'Jumlah',
     'Your order list is empty' => 'Daftar pesanan anda kosong',
-    'Total Order' => 'Total Pemesanan',
     'Write a Note' => 'Tuliskan Keterangan',
     'Go to Checkout' => 'Lanjut ke Checkout',
     'Order This' => 'Pesan Ini',
+    'Order Now' => 'Pesan Sekarang',
     
     'Coming Soon' => 'Segera Hadir',
     'Search menu category here' => 'Cari kategori menu disini'
