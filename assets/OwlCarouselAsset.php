e11bae0e6bc44efb29b609f4e0415534b8a622d2
AxelRj13 <axel.raharja1@gmail.com>
2019-03-18 18:10:36 +0700

    Penambahan file asset untuk owl carousel

---
+++ assets/OwlCarouselAsset.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace common\assets;
+
+use yii\web\AssetBundle;
+
+class OwlCarouselAsset extends AssetBundle {
+    
+    public $sourcePath = '@bower/owl.carousel';
+    
+    public $css = [
+        'dist/assets/owl.carousel.min.css',
+        'dist/assets/owl.theme.default.min.css',
+    ];
+    public $js = [
+        'dist/owl.carousel.min.js',
+    ];
+    public $depends = [
+        'yii\web\JqueryAsset',
+    ];
+    
+}
