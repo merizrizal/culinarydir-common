81988c5fccb4a29f1255aecf6f0b38ef1897536f
Mei Rizal <meriz.rizal@gmail.com>
2019-06-21 22:54:43 +0700

    Pemnambahan bot

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -5,6 +5,7 @@
 \Yii::setAlias('frontend', dirname(dirname(__DIR__)) . '/frontend');
 \Yii::setAlias('api', dirname(dirname(__DIR__)) . '/api');
 \Yii::setAlias('webview', dirname(dirname(__DIR__)) . '/webview');
+\Yii::setAlias('bot', dirname(dirname(__DIR__)) . '/bot');
 \Yii::setAlias('console', dirname(dirname(__DIR__)) . '/console');
 
 \Yii::setAlias('root', dirname(dirname(dirname(__DIR__))));
